9954ea0510dcc8f4897997eb53ce0f8d87c0f463
Snafuzima <kauffman1613@gmail.com>
2021-08-27 03:42:57 +0300

    Создал стили, вынес dimens размеры, поменял button на material button, сделал один класс для всех clicklistener`ов, сделал темную тему,

--- app/src/main/java/com/example/calculator_220821/domain/Operations.java
+++ app/src/main/java/com/example/calculator_220821/domain/Operations.java
@@ -9,21 +9,20 @@ public class Operations {
     }
 
     public void plus(float firstValue, float secondValue) {
-        result=firstValue+secondValue;
+        result = firstValue + secondValue;
     }
 
     public void minus(float firstValue, float secondValue) {
-        result=firstValue-secondValue;
+        result = firstValue - secondValue;
     }
 
     public void multi(float firstValue, float secondValue) {
-        result=firstValue*secondValue;
+        result = firstValue * secondValue;
     }
 
     public void div(float firstValue, float secondValue) {
-        result=firstValue/secondValue;
+        result = firstValue / secondValue;
     }
-
 
 
 }
